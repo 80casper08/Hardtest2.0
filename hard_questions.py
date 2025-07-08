880ecc3aa5203d48e8b9ed33bd65a81a0fc646fe
80casper08 <pavlo20202020@gmail.com>
2025-07-08 18:06:42 +0300

    hard_questions.py

---
+++ hard_questions.py
@@ -0,0 +1,24 @@
+
+hard_questions = [
+    {
+        "text": "Яких елементів не вистачає на платі KeyPad?",
+        "options": [
+            ("Холдер \"+\"", True),
+            ("Світлодіод", True),
+            ("Резистор", False),
+            ("Холдер \"-\"", True)
+        ],
+        "image": "https://raw.githubusercontent.com/80casper08/Hardtest2.0/main/images/1.jpg"
+    },
+    {
+        "text": "Що потрібно перевірити перед прошивкою пристрою?",
+        "options": [
+            ("Наявність живлення", True),
+            ("Підключення UART кабелю", True),
+            ("Колір корпусу", False),
+            ("Встановлення гвинтів", False)
+        ],
+        "image": "https://raw.githubusercontent.com/80casper08/Hardtest2.0/main/images/2.jpg"
+    }
+]
+
