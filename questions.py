f79ee20aec13312dc17faaf123981001dc41649a
80casper08 <pavlo20202020@gmail.com>
2025-07-08 11:07:39 +0300

    questions.py

--- questions.py
+++ questions.py
@@ -1428,4 +1428,27 @@ qr_questions += [
         ]
     },
 ]
-hard_questions = []
+hard_questions = [
+    {
+        "text": "Яких елементів не вистачає на платі KeyPad?",
+        "image": "images/1.jpg",
+        "options": [
+            ("Холдер \"-\"", True),
+            ("Резистор", False),
+            ("Світлодіод", True),
+            ("Холдер \"+\"", True)
+        ]
+    },
+    {
+        "text": "Яких елементів не вистачає на платі StreetSiren?",
+        "image": "images/2.jpg",
+        "options": [
+            ("Антена", True),
+            ("Світлодіод", False),
+            ("Кнопка", True),
+            ("Кварцовий резонатор", True)
+        ]
+    }
+]
+
+
